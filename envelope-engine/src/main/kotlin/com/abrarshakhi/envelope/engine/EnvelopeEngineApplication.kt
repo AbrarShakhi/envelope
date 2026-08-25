4dd77cc3c0578a725ff0470ce967a3980bb57201
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-08-25 14:45:12 +0600

    server initialized

---
+++ envelope-engine/src/main/kotlin/com/abrarshakhi/envelope/engine/EnvelopeEngineApplication.kt
@@ -0,0 +1,11 @@
+package com.abrarshakhi.envelope.engine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnvelopeEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<EnvelopeEngineApplication>(*args)
+}
---
+++ envelope-engine/src/test/kotlin/com/abrarshakhi/envelope/engine/EnvelopeEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abrarshakhi.envelope.engine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EnvelopeEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
